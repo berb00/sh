c80e7262e01c1d2c35499d4625b29ea6653992cd
berb00 <berb11@163.com>
2020-04-29 21:01:51 +0800

    -

--- operator.sh
+++ operator.sh
@@ -12,34 +12,46 @@ b=20
 
 val=`expr 2 + 2`            # 表达式之间要有空格 2+2错误
 
+# ======================算术运算符======================
 add=`expr $a + $b`
-echo "a + b : ${add}"
+echo "$a + $b : ${add}"
 
 sub=`expr $a - $b`
-echo "a - b : ${sub}"
+echo "$a - $b : ${sub}"
 
 multiply=`expr $a \* $b`
-echo "a * b : ${multiply}"
+echo "$a * $b : ${multiply}"
 
 divide=`expr $a / $b`
-echo "a / b : ${add}"
+echo "$a / $b : ${divide}"
 
 modle=`expr $a % $b`
-echo "a % b : ${add}"
+echo "$a % $b : ${modle}"
 
 equal=`expr $a == $b`
-echo "a == b : ${equal}"
+echo "$a == $b : ${equal}"
 
 notequal=`expr $a != $b`
-echo "a != b : ${notequal}"
+echo "$a != $b : ${notequal}"
+
+
+# ======================关系运算符======================
+echo "==============================="
+
+if [ $a -eq $b ]; then
+    echo "-eq : $a == $b"
+else
+    echo "-eq : $a != $b"
+fi
 
 
 
-
-
-
-
-
+if [ $a -le $b ]
+then
+   echo "$a -le $b: a 小于或等于 b"
+else
+   echo "$a -le $b: a 大于 b"
+fi
 
 
 
